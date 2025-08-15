040ff8672dcdb3a88bb3eb36440e1decf40be035
Hamed Razavi <h.razavi89@gmail.com>
2025-08-15 14:35:58 +0200

    updates

--- Results_psnr/run.bat
+++ Results_psnr/run.bat
@@ -1,4 +1,4 @@
 @echo off
 
-IV_PSNR.exe -i0 "../../../../dataset/ETRI/images_2/00.yuv" -i1 "000.yuv" -w 860 -h 540 -bd 8 -cf 420 -o "00.txt"
+IV_PSNR.exe -i0 "GT_00.yuv" -i1 "000.yuv" -w 860 -h 540 -bd 8 -cf 420 -o "00.txt"
 
--- Results_psnr/run_psnr.bat
+++ Results_psnr/run_psnr.bat
@@ -1,8 +1,13 @@
 @echo off
-set output=all_results.txt
+set output=random_results.txt
 del %output% 2>nul
 
-python .\psnr.py .\GT_00.png .\prop_scaled_loss.png %output%
+python .\psnr.py .\GT_00.png .\random_00.png %output%
+python .\psnr.py .\GT_00.png .\100_00.png %output%
+python .\psnr.py .\GT_00.png .\150_00.png %output%
+python .\psnr.py .\GT_12.png .\random_12.png %output%
+python .\psnr.py .\GT_12.png .\100_12.png %output%
+python .\psnr.py .\GT_12.png .\150_12.png %output%
 
 echo All PSNR results saved to %output%
 pause
