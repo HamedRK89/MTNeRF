e7cc986a4ec165abea50a89e45ddb07c6c6a33d2
Hamed Razavi <h.razavi89@gmail.com>
2025-08-25 17:44:00 +0200

    before_merge

--- Results_psnr/run_psnr.bat
+++ Results_psnr/run_psnr.bat
@@ -2,12 +2,15 @@
 set output=random_results.txt
 del %output% 2>nul
 
-python .\psnr.py .\GT_00.png .\random_00.png %output%
-python .\psnr.py .\GT_00.png .\100_00.png %output%
-python .\psnr.py .\GT_00.png .\150_00.png %output%
-python .\psnr.py .\GT_12.png .\random_12.png %output%
-python .\psnr.py .\GT_12.png .\100_12.png %output%
-python .\psnr.py .\GT_12.png .\150_12.png %output%
+python .\psnr.py .\GT_00.png .\00_baseline.png %output%
+python .\psnr.py .\GT_00.png .\00_1.0.png %output%
+python .\psnr.py .\GT_00.png .\00_0.9.png %output%
+python .\psnr.py .\GT_00.png .\00_0.8.png %output%
+
+python .\psnr.py .\GT_12.png .\12_baseline.png %output%
+python .\psnr.py .\GT_12.png .\12_1.0.png %output%
+python .\psnr.py .\GT_12.png .\12_0.9.png %output%
+python .\psnr.py .\GT_12.png .\12_0.8.png %output%
 
 echo All PSNR results saved to %output%
 pause
