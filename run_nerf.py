dbda546cb5763b3d9a150ff2739e2e76f8ee6822
Farnazfrl <faramarzi.f93@gmail.com>
2025-08-27 13:08:28 +0200

    split_head_added

--- run_nerf.py
+++ run_nerf.py
@@ -17,6 +17,7 @@ from load_llff import load_llff_data
 from load_deepvoxels import load_dv_data
 from load_blender import load_blender_data
 from load_LINEMOD import load_LINEMOD_data
+from split_head_nerf import SplitLayerNeRF
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
